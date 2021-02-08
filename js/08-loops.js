450a7fcece21b50f5005200921cee5867463f78b
VasylBarna <vasylbarna@gmail.com>
2021-02-08 16:58:55 +0200

    repeta 2(34hv.)

---
+++ js/08-loops.js
@@ -0,0 +1,19 @@
+
+// for (let count = 0; count < 5; count += 1) { console.log(`Count ${count}`); } 
+
+
+// for (let i = 10; i > 0; i -= 1) { console.log(`i ${i}`); } 
+
+
+// let userInput = prompt("Введите число");
+// userInput = Number(userInput);
+// let total = 0;
+// for (let i = 1; i <= userInput; i += 1) { total += i; }
+// console.log(`Total = ${total}`);
+
+
+const number = 5;
+let total = 1;
+while (condition) {
+              
+}
