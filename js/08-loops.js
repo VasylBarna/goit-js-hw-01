59d35e56792b45790f98d8d82c256fb542e02806
VasylBarna <vasylbarna@gmail.com>
2021-02-10 15:00:40 +0200

    js repeta modul 1

--- js/08-loops.js
+++ js/08-loops.js
@@ -12,8 +12,35 @@
 // console.log(`Total = ${total}`);
 
 
-const number = 5;
-let total = 1;
-while (condition) {
-              
-}
+// const number = 3;
+// let total = 0;
+// let i = 1;
+
+// while(i <= number) {
+//               total += i;
+//               i += 1;           
+// }
+// console.log(`Total = ${total}`);
+// i = 1;
+// total = 0 + 1 = 1;
+// i = 2;
+// total = 1 + 2 = 3;
+// i = 3;
+// total = 3 + 3 = 6;
+
+
+// let userInput;
+// do {
+//               userInput = prompt("Введи 5");
+//               if (userInput === null) { console.log("Виход"); break; }
+//               userInput = Number(userInput);
+// } while (userInput !== 5);
+// console.log(userInput);
+
+// for (let i = 0; i < 10; i += 1)
+// if ( i% 2 ===0 ) 
+//               { console.log(i); }
+
+// for (let i = 0; i < 10; i += 1) {
+//               if (i % 2 === 0) { continue;}
+//  console.log(i); }
