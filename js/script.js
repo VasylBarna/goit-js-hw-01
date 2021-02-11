fdb29521e60adf12d234022447d30fa12479afbe
VasylBarna <vasylbarna@gmail.com>
2021-02-11 16:52:15 +0200

    1urok

---
+++ js/script.js
@@ -0,0 +1,80 @@
+// const num = parseFloat('25.7px');
+// console.log(num);
+
+
+// const a = 300;
+// function exampleFunc() {
+//   console.log(a);
+// }
+// function exampleFunc2() {
+//   console.log(a);
+// }
+// exampleFunc();
+
+
+// const a = 12;
+// if (a % 2 && a % 3) {
+//   console.log('не четная');
+// } else {
+//   console.log('четная и делится на 3');
+// }
+
+
+// function sumWithSomething(a, b) {
+//               return (a + b) * 10;
+// }
+// function print(text) {
+//   console.log(text);
+// }
+// const val4 = sumWithSomething(1, 3);
+// print(val4);
+// const val5 = sumWithSomething(2, 3);
+// print(val5);
+
+
+// let i = 2;
+// console.log(--i);
+// console.log(i);
+
+
+// Обьект Math
+// https://www.w3schools.com/js/js_math.asp
+// const rad = 10;
+// const area = Math.PI * Math.pow(rad, 2);
+// console.log(area);
+
+
+  
+// Шаблонные строки и конкатенация строк. Методы строк.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+// const name = 'Манго';
+// const age = 5;
+// const hobby = 'плавать';
+// // const messageConcat = ' Меня зовут ' + name + ', мне ' + age + hobby;
+// const messageSchema = ` Меня зовут ${name}, мне ${age} лет и я люблю ${hobby}`;
+// console.log(messageSchema[1]);
+
+
+// console.log('"200" < "120"');
+// console.log('2' < '12');
+// console.log("'Ёнанас' < 'ёжик'");
+// console.log('Ёнанас' < 'ёжик');
+
+
+// const total = 430;
+// const hours = Math.floor(total / 60);
+// const minutes = total % 60;
+
+// const doubleDigitHours = `${hours}`.padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+
+// console.log(hours);
+// console.log(minutes);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+
+// let text = 'gerge';
+// let boolText = !!text;
+// console.log(text);
+// if (text !== '123') {
+//   console.log('not equal');
+// }
